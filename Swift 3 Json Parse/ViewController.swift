1037482f23b72b5f0734771c75a6d3aa69ed330f
Muhammed Esad Kaya <mesadkaya@Muhammed-MacBook-Pro.local>
2017-09-02 02:50:06 +0300

    Initial Commit

---
+++ Swift 3 Json Parse/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift 3 Json Parse
+//
+//  Created by Muhammed Esad Kaya on 02/09/2017.
+//  Copyright © 2017 Muhammed Esad Kaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
