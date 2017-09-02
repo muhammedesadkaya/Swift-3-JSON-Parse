9305c10060c764a51a1f544115498f3eb3ddc319
Muhammed Esad Kaya <mesadkaya@Muhammed-MacBook-Pro.local>
2017-09-02 03:15:02 +0300

    First commit.

--- Swift 3 Json Parse/ViewController.swift
+++ Swift 3 Json Parse/ViewController.swift
@@ -10,15 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let urlString = "http://www.bucayapimarket.com/json.php";
+        let myurl = URL(string: urlString)
+        
+        let task = URLSession.shared.dataTask(with: myurl!) { (data, response, error) in
+            
+            if error != nil //if there is a error
+            {
+                print(error!)
+            }
+            else
+            {
+                do
+                {
+                    let myjson = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as! NSArray
+                    
+                    print(myjson)
+                }
+                catch
+                {
+                    print(error)
+                }
+            }
+        }
+        task.resume()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+            }
 
 
 }
